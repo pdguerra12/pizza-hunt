4894938d2fbeaa84afe2e4d4d94afb430a734075
pdguerra12 <paul.guerra09@gmail.com>
2022-03-01 16:28:37 -0600

    comment model completed

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,14 @@ const pizzaController = {
 	// get all pizzas
 	getAllPizza(req, res) {
 		Pizza.find({})
+			.populate({
+				path: "comments",
+				select: "-__v",
+			})
+			// the "-" below and above mean not to include this data
+			.select("-__v")
+			// sorting the pizzas in DESC order with the newest pizza returns first.
+			.sort({ _id: -1 })
 			.then((dbPizzaData) => res.json(dbPizzaData))
 			.catch((err) => {
 				console.log(err);
@@ -14,6 +22,11 @@ const pizzaController = {
 	// get one pizza by id
 	getPizzaById({ params }, res) {
 		Pizza.findOne({ _id: params.id })
+			.populate({
+				path: "comments",
+				select: "-__v",
+			})
+			.select("-__v")
 			.then((dbPizzaData) => {
 				// If no pizza is found, send 404
 				if (!dbPizzaData) {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,45 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const PizzaSchema = new Schema({
-	pizzaName: {
-		type: String,
+const PizzaSchema = new Schema(
+	{
+		pizzaName: {
+			type: String,
+		},
+		createdBy: {
+			type: String,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			// 'get' below gets the data and cleans it up to look more presentable.
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+		size: {
+			type: String,
+			default: "Large",
+		},
+		toppings: [],
+		comments: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Comment",
+			},
+		],
 	},
-	createdBy: {
-		type: String,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-	size: {
-		type: String,
-		default: "Large",
-	},
-	toppings: [],
+	// telling the schema it can use virtuals
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+		id: false,
+	}
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+	return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
